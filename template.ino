68380e6c73cd8c8defbbb8f56572ad6e8ad4c6b5
Tjark Ziehm <me@tjarkziehm.de>
2024-04-19 20:00:07 +0200

    upload template files

---
+++ template.ino
@@ -0,0 +1,29 @@
+/**
+ * @file DoxygenExample.ino
+ *
+ * @mainpage Doxygen Example Project
+ *
+ * @section description Description
+ * An example sketch demonstrating how to use Doxygen style comments for
+ * generating source code documentation with Doxygen.
+ *
+ * @section circuit Circuit
+ * - Red LED connected to pin D2.
+ * - Momentary push button connected to pin D3.
+ *
+ * @section libraries Libraries
+ * - Arduino_LSM6DS3 (https://github.com/arduino-libraries/Arduino_LSM6DS3)
+ *   - Interacts with on-board IMU.
+ *
+ * @section notes Notes
+ * - Comments are Doxygen compatible.
+ *
+ * @section todo TODO
+ * - Don't use Doxygen style formatting inside the body of a function.
+ *
+ * @section author Author
+ * - Created by John Woolsey on 03/12/2020.
+ * - Modified by John Woolsey on 03/16/2020.
+ *
+ * Copyright (c) 2020 Woolsey Workshop.  All rights reserved.
+ */
---
+++ template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#######################################################
+### ****.py: What is the task of the code. ( in Short )
+# General Text and Notice
+#######################################################
+
+"""
+NumberList holds a sequence of numbers, and defines several statistical
+operations (mean, stdev, etc.) FrequencyDistribution
+holds a mapping from items (not necessarily numbers)
+to counts, and defines operations such as Shannon
+entropy and frequency normalization.
+"""
+
+__date__ = "18.03.2024"
+__author__ = "Tjark Ziehm"
+__copyright__ = "Copyright 2024, BalticMaterials"
+__credits__ = ["Tjark Ziehm"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Tjark Ziehm"
+__email__ = "kontakt@balticmaterials.de"
+__status__ = "Development"
